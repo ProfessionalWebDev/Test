3d52b83584e5ece010c03e10636393aa795732f1
Anirudh <darklord1911@gmail.com>
2016-01-19 14:59:10 +0530

    Resoved the BUG of req.body
    Needed to parse body in order to get request from the DB ...

--- Server/routes.js
+++ Server/routes.js
@@ -1,5 +1,9 @@
 module.exports = function (app){
 	console.log("Inside Routes");
+	// declare body parser
+	var bodyParser = require('body-parser');
+	// parse application/json
+	app.use(bodyParser.json());
 	
 	app.get('/details', require('./details/details').getDetails);	
 	app.get('/details/:id', require('./details/details').getDetails_id);
